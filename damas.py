7354b508d6d5ffd53216a1cbf921eb9d463b5eff
Ereos <ereos.it@gmail.com>
2014-03-18 12:59:54 +0100

    Pedir nombre jugadores y almacenar sus movimientos

--- damas.py
+++ damas.py
@@ -1,7 +1,25 @@
-_author__ = 'dcubero'
+# -*- coding:uft-8 -*-
+_author__ = 'dcubero, ddiaz'
 
 print ('Bienvenidos al juego de las damas!')
-playerA = raw_input('Introduce el nombre del jugador A: ')
-playerB = raw_input('Introduce el nombre del jugador B: ')
+jugadorA = raw_input('Introduce el nombre del jugador A: ')
+jugadorB = raw_input('Introduce el nombre del jugador B: ')
+listaA = []
+listaB = []
 
-print("\n El primero en jugar es el jugador ",playerA)
+
+print("\n El primero en mover es el jugador " + jugadorA)
+movimiento = 'FF'
+
+
+while (movimiento != '00'):
+
+    movimiento = raw_input('Introduce un movimiento ' + jugadorA + ': ')
+    listaA.append(movimiento)
+
+    movimiento = raw_input('Introduce un movimiento ' + jugadorB + ': ')
+    listaB.append(movimiento)
+
+
+print (listaA)
+print (listaB)
