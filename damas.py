d5cc9b458a1932165d4f8752cdbf9d13bd630d68
Ereos <Ereos@users.noreply.github.com>
2014-03-18 12:32:25 +0100

    Create damas.py

---
+++ damas.py
@@ -0,0 +1,7 @@
+_author__ = 'dcubero'
+
+print ('Bienvenidos al juego de las damas!')
+playerA = raw_input('Introduce el nombre del jugador A: ')
+playerB = raw_input('Introduce el nombre del jugador B: ')
+
+print("\n El primero en jugar es el jugador ",playerA)
