780c31fda41b0ab15adc27a708f1baf1db5ede38
Ereos <ereos.it@gmail.com>
2014-03-19 21:13:20 +0100

    Añadido infromacion sobre el programa

--- damas.py
+++ damas.py
@@ -1,5 +1,16 @@
-# -*- coding:uft-8 -*-
-_author__ = 'dcubero, ddiaz'
+#!/usr/bin/python
+#coding=utf-8
+#
+#Titulo:
+#Damas-python
+#
+#Autor:
+#dcubero
+#diaz
+#
+#Descripcion:
+#Juego de las damas desarrollando en python
+
 
 print ('Bienvenidos al juego de las damas!')
 jugadorA = raw_input('Introduce el nombre del jugador A: ')
@@ -12,7 +23,7 @@ print("\n El primero en mover es el jugador " + jugadorA)
 movimiento = 'FF'
 
 
-while (movimiento != '00'):
+while movimiento != '00':
 
     movimiento = raw_input('Introduce un movimiento ' + jugadorA + ': ')
     listaA.append(movimiento)
@@ -21,6 +32,12 @@ while (movimiento != '00'):
     listaB.append(movimiento)
 
 
+def convertirMovimientos(mov):
+    letras={"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7}
+
+
+
 
 print (listaA)
 print (listaB)
+
