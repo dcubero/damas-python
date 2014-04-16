83aa946159fa34ee35c0696dd216ec3a40977ded
Ereos <ereos.it@gmail.com>
2014-04-16 18:09:06 +0200

    Movimiento de las fichas NO

--- damas.py
+++ damas.py
@@ -13,43 +13,62 @@
 
 
 #Definicion de variables
-tablero = [[0, 1, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 0, 0, 0, 0], [2, 0, 2, 0, 2, 0, 2, 0], [0, 2, 0, 2, 0, 2, 0, 2], [2, 0, 2, 0, 2, 0, 2, 0]]
+tablero = [['-', 'b', '-', 'b', '-', 'b', '-', 'b'],
+           ['b', '-', 'b', '-', 'b', '-', 'b', '-'],
+           ['-', 'b', '-', 'b', '-', 'b', '-', 'b'],
+           ['-', '-', '-', '-', '-', '-', '-', '-'],
+           ['-', '-', '-', '-', '-', '-', '-', '-'],
+           ['n', '-', 'n', '-', 'n', '-', 'n', '-'],
+           ['-', 'n', '-', 'n', '-', 'n', '-', 'n'],
+           ['n', '-', 'n', '-', 'n', '-', 'n', '-']]
 
 letras = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 movimientoValido = True
+jugador = 'b'
 
 
 
 
 #Definicion de metodos y funciones
-def moValido(jugada):
+def moValido(jugada, colorJugador):
     """
 	Comprueba si la jugada es valida
 	"""
+    numero = 0
 
     if jugada[0].upper() not in letras:
+        print numero
         return False
 
     elif jugada[2].upper() not in letras:
+        print numero
         return False
 
     elif jugada[1] > 8 or jugada[1] < 1:
+        print numero
         return False
 
     elif jugada[3] > 8 or jugada[3] < 1:
+        print numero
         return False
 
     else:
-        movOriRow=letras[jug[0]]
-        movOriCol=int(jug[1])-1
-        movDesRow=letras[jug[2]]
-        movDesCol=int(jug[3])-1
+        movOriRow=letras[jugada[0]]
+        movOriCol=int(jugada[1])-1
+        movDesRow=letras[jugada[2]]
+        movDesCol=int(jugada[3])-1
+
+        if tablero[movOriRow][movOriCol] == 0:
+            return False
 
         ficha = tablero[movOriRow][movOriCol]
 
-        if (ficha.tipo == "dama" ):
+        if ficha != colorJugador:
+            return False
 
+        if (ficha == 'N') or (ficha == 'B'):
+
+            #Logica de la dama
             return True
         else:
             #Jugada adyacente
@@ -57,59 +76,68 @@ def moValido(jugada):
                     (movDesRow == movOriRow + 1 and movDesCol == movOriCol - 1) or
                     (movDesRow == movOriRow - 1 and movDesCol == movOriCol + 1) or
                     (movDesRow == movOriRow - 1 and movDesCol == movOriCol - 1)):
+
+                fichaDestino = tablero[movDesRow][movDesCol]
+
+                if fichaDestino == ficha:
+                    return False
+
                 return True
 
             else:
                 return False
 
+def moverFicha (jugada, colorJugada):
+    movOriRow=letras[jugada[0]]
+    movOriCol=int(jugada[1])-1
+    movDesRow=letras[jugada[2]]
+    movDesCol=int(jugada[3])-1
 
+    fichaOrigen = tablero[movOriRow][movOriCol]
+    fichaDestino = tablero [movDesRow][movDesCol]
 
+    if fichaDestino == '-':
+        tablero[movDesRow][movDesCol] = fichaOrigen
+        tablero[movOriRow][movOriCol] = '-'
+
+    elif fichaDestino != colorJugada:
+
+        tablero[movOriRow][movOriCol] = '-'
+        if movDesRow < movOriRow:
+            if movDesCol < movOriCol:
+                tablero[movDesRow - 1][movDesCol - 1] = fichaOrigen
+
+            else:
+               tablero[movDesRow - 1][movDesCol + 1] = fichaOrigen
+
+        else:
+            if movDesCol < movOriCol:
+                tablero[movDesRow + 1][movDesCol - 1] = fichaOrigen
+
+            else:
+                tablero[movDesRow + 1][movDesCol + 1] = fichaOrigen
 
 
 print 'Bienvenidos al juego de las damas'
+print 'Comienzan las blancas'
 
 while movimientoValido:
+
     movimiento = raw_input()
 
+    if  moValido(movimiento, jugador):
+        movimientoValido = True
+        moverFicha(movimiento, jugador)
 
+        print tablero[7]
+        print tablero[6]
+        print tablero[5]
+        print tablero[4]
+        print tablero[3]
+        print tablero[2]
+        print tablero[1]
+        print tablero[0]
 
-    movimientoValido = moValido(movimiento)
-
-    print tablero[7]
-    print tablero[6]
-    print tablero[5]
-    print tablero[4]
-    print tablero[3]
-    print tablero[2]
-    print tablero[1]
-    print tablero[0]
-
-
-
-#Definicion de clases
-class Ficha:
-    color = ''
-    posicion = 0
-    tipo = ''
-
-
-    def __init__(self, color, posicion, tipo):
-        self.color = color
-        self.posicion = posicion
-        self.tipo = tipo
-
-
-    def getColor(self):
-        return self.color
-
-    def setColor(self, color):
-        self.color = color
-
-    def getPosicion(self):
-        return self.posicion
-
-    def setPosicion(self, poscion):
-        self.posicion = poscion
-
-    def getTipo(self):
-        return self.tipo
+    else:
+        movimientoValido = False
+        print 'Movimiento no válido'
